d3ba6d5cd7b998e67fa4cf66146ab08de212b0fe
FREITAS ROSADA Marcos Ruggeri <mrfreitasrosada@europe.corp.altran.com>
2019-09-10 17:03:47 +0100

    Node Modules Import

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 // app.get('/', function(req, res) {
 //     res.send('Hello from server');
