947e43c520983605712b3ad0445deae19f54f25b
FREITAS ROSADA Marcos Ruggeri <mrfreitasrosada@europe.corp.altran.com>
2019-09-10 14:41:35 +0100

    Serving Index

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express = require('express'),
     // chalk = require('chalk'),
     morgan = require('morgan'),
-    debug = require('debug')('app');
+    debug = require('debug')('app'),
+    path = require('path');
 
 var app = express();
 
 app.use(morgan('tiny'));
 
+// app.get('/', function(req, res) {
+//     res.send('Hello from server');
+// });
+
 app.get('/', function(req, res) {
-    res.send('Hello from server');
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen('3000', function() {
