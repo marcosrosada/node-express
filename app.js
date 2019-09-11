564e836d2294cb2032de2e76289bf06b948780dd
FREITAS ROSADA Marcos Ruggeri <mrfreitasrosada@europe.corp.altran.com>
2019-09-11 11:31:55 +0100

    ESlint

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express'),
-    // chalk = require('chalk'),
-    morgan = require('morgan'),
-    debug = require('debug')('app'),
-    path = require('path');
+const express = require('express');
+// const chalk = require('chalk');
+const morgan = require('morgan');
+const debug = require('debug')('app');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -16,10 +16,10 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 //     res.send('Hello from server');
 // });
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen('3000', function() {
-    debug('App running on port 3000');
+app.listen('3000', () => {
+  debug('App running on port 3000');
 });
