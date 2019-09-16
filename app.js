1295ae2b0ebe24cfc2c975ebf4b1298e6deaa3e5
Marcos R F Rosada <mrfreitasrosada@europe.corp.altran.com>
2019-09-16 11:55:59 +0100

    Routing Refactory

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const bookRouter = require('./src/routes/bookRoutes');
+
+const nav = [
+  { link: '/books', title: 'Books' },
+  { link: '/authors', title: 'Authors' }
+];
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -27,10 +33,7 @@ app.get('/', (_req, res) => {
   res.render('index',
     {
       title: 'Marcos Rosada',
-      nav: [
-        { link: '/books', title: 'Books' },
-        { link: '/authors', title: 'Authors' }
-      ]
+      nav
     });
 });
 
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,73 +1,72 @@
 const express = require('express');
 
 const bookRouter = express.Router();
-const books = [
-  {
-    id: 1,
-    title: 'Title A',
-    genre: 'Historical',
-    author: 'Lev',
-    read: false
-  },
-  {
-    id: 2,
-    title: 'Title B',
-    genre: 'Historical',
-    author: 'Lev',
-    read: false
-  },
-  {
-    id: 3,
-    title: 'Title C',
-    genre: 'Historical',
-    author: 'Lev',
-    read: false
-  },
-  {
-    id: 4,
-    title: 'Title D',
-    genre: 'Historical',
-    author: 'Lev',
-    read: false
-  },
-  {
-    id: 5,
-    title: 'Title F',
-    genre: 'Historical',
-    author: 'Lev',
-    read: false
-  }
-];
 
-bookRouter.route('/')
-  .get((req, res) => {
-    res.render(
-      'bookListView',
-      {
-        title: 'Marcos Rosada',
-        nav: [
-          { link: '/books', title: 'Books' },
-          { link: '/authors', title: 'Authors' }
-        ],
-        books
-      }
-    );
-  });
+function router(nav) {
+  const books = [
+    {
+      id: 1,
+      title: 'Title A',
+      genre: 'Historical',
+      author: 'Lev',
+      read: false
+    },
+    {
+      id: 2,
+      title: 'Title B',
+      genre: 'Historical',
+      author: 'Lev',
+      read: false
+    },
+    {
+      id: 3,
+      title: 'Title C',
+      genre: 'Historical',
+      author: 'Lev',
+      read: false
+    },
+    {
+      id: 4,
+      title: 'Title D',
+      genre: 'Historical',
+      author: 'Lev',
+      read: false
+    },
+    {
+      id: 5,
+      title: 'Title F',
+      genre: 'Historical',
+      author: 'Lev',
+      read: false
+    }
+  ];
 
-bookRouter.route('/:id')
-  .get((req, res) => {
-    const { id } = req.params;
-    res.render(
-      'bookView',
-      {
-        title: 'Marcos Rosada',
-        nav: [
-          { link: '/books', title: 'Books' },
-          { link: '/authors', title: 'Authors' }
-        ],
-        book: books[id]
-      }
-    );
-  });
+  bookRouter.route('/')
+    .get((req, res) => {
+      res.render(
+        'bookListView',
+        {
+          title: 'Marcos Rosada',
+          nav,
+          books
+        }
+      );
+    });
 
-module.exports = bookRouter;
+  bookRouter.route('/:id')
+    .get((req, res) => {
+      const { id } = req.params;
+      res.render(
+        'bookView',
+        {
+          title: 'Marcos Rosada',
+          nav,
+          book: books[id]
+        }
+      );
+    });
+
+  return bookRouter;
+}
+
+module.exports = router;
